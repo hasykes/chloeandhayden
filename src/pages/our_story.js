ddf6870a86f97c1417b2f099b642858331d0f65d
Hayden Sykes <haydensykes13@gmail.com>
2019-11-23 16:10:37 -0500

    add spotify player and youtube link to our_story

--- src/pages/our_story.js
+++ src/pages/our_story.js
@@ -1,10 +1,11 @@
 import React from "react"
-//import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import AboutCard from "../components/aboutCard"
 
+const SpotifyWrapper = styled.div``
 const OurStory = () => (
   <Layout>
     <SEO title="Our Story" />
@@ -20,6 +21,17 @@ const OurStory = () => (
       about="This cutie patootie has the finest booty you ever seen"
       src="https://scontent-atl3-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/69878654_546039236140580_6409408774553611869_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com&_nc_cat=108&oh=9664c3e8a99d4d7c3068956c9c4a1fa9&oe=5E5904A4"
     />
+    <SpotifyWrapper>
+      <iframe
+        src="https://open.spotify.com/embed/track/74SFqzOS8Z0rbbG2llSVaQ"
+        width="300"
+        height="80"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      />
+      <a href="" target="_blank" rel="noopener noreferer"></a>
+    </SpotifyWrapper>
   </Layout>
 )
 
