0a4820c35631b45e59e1a33e4976d84731c7cd45
Hayden Sykes <haydensykes13@gmail.com>
2020-10-24 13:43:32 -0400

    basic outline and framing of new site style for the site.  A lot more simplistic.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout'
-import BillboardImage from '../components/billboardImage'
-import ModernFlair from '../components/modernFlair'
-import CallToAction from '../components/callToAction'
-import SubPage from '../components/subPage'
-import Title from '../components/title'
 //import SEO from '../components/seo'
 import styled from 'styled-components'
 import { theme } from '../components/theme'
 
+const FlexContainer = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  align-content:center;
+
+  min-height:100vh;
+`
 const ContentWrapper = styled.div`
   position:relative;
-  width:100vw;
-  height:100vh;
+  width:90vw;
+  height:90vh;
+  background-color: ${theme.primaryBackground};
+  color:${theme.primaryText};
+  flex:0 1 auto;
 
   overflow:hidden;
-
 
   /*Mobile Styles*/
   @media (max-width: ${theme.devices.tablet}px),(orientation: portrait)  {
@@ -25,6 +30,79 @@ const ContentWrapper = styled.div`
       width:100vw;
       height:100vh;
     }
+`
+
+const Flex = styled.div`
+
+ display:flex;
+  width:100%;
+
+  justify-content:center;
+ 
+`
+
+const Header = styled.h1`
+
+  width:100%; 
+  text-align:center;
+  margin-top:.75em;
+`
+const Date = styled.div`
+  position:absolute;
+  top:50%;
+  left:-25px;
+  transform:rotate(-90deg);
+  
+`
+
+const DiscoBall = styled.img`
+
+  position:absolute;
+  top:60%;
+  left:50%;
+  transform: translate(-50%, -50%);
+  margin:0;
+
+`
+
+const Hashtag = styled.div`
+
+position:absolute;
+  top:50%;
+  right:-40px;
+transform:rotate(90deg);
+`
+const RsvpButton = styled.button`
+  position:absolute;
+  bottom:-20px;
+  left:50%;
+  transform: translate(-50%, -50%);
+  width:100px;
+  height:50px;
+  background-color:${theme.primaryAccent};
+  border:none;
+
+  color:${theme.primaryBackground};
+  font-weight:bold;
+`
+
+const GridWrap = styled.div`
+  display:grid;
+  grid-template-columns:repeat(2,1fr);
+  grid-template-rows:repeat(2,1fr);
+
+  height:100%;
+  width:100%;
+`
+
+const LightBox = styled.div`
+  background-color:transparent;
+  order:${props => props.order};
+`
+
+const DarkBox = styled.div`
+  background-color:${theme.secondaryBackground};
+  order:${props => props.order};
 `
 
 class IndexPage extends React.Component {
@@ -37,52 +115,29 @@ class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <ContentWrapper id='home' >
-          <ModernFlair color='peach' type='bottom-right' mobileType='top' zindex={0} />
-          <ModernFlair color='blue' type='bottom-left' mobileType='middle' zindex={-1}/>
-          <ModernFlair color='terra' type='top-center' mobileType='bottom' zindex={0}/>
-          <Title title='Chloe and Hayden' /> 
-          <BillboardImage/>
-        </ContentWrapper>
-        <ContentWrapper id='about' >
-          <SubPage content='about'/>
-        </ContentWrapper>
-        <ContentWrapper id='registry'>
-          <SubPage content='registry'/>
-        </ContentWrapper>
+        <FlexContainer>
+          <ContentWrapper id='home' >
+            <Header>Chloe & Hayden</Header>
+            
+              <Date>March 27, 2021</Date>
+              <DiscoBall src="https://static.wixstatic.com/media/19e8d8_26a98e6e68534a9da800d6718b976381~mv2_d_3508_4961_s_4_2.png/v1/crop/x_181,y_152,w_3327,h_3922/fill/w_480,h_570,al_c,q_85,usm_0.66_1.00_0.01/light%20green%20disco.webp" alt="groovy disco ball" />
+              <Hashtag>#MartinDownTheAisle</Hashtag>
+              <RsvpButton>RSVP</RsvpButton>
+          </ContentWrapper>
+        </FlexContainer>
+        <FlexContainer>
+          <ContentWrapper id='info' >
+            <GridWrap>
+            <LightBox order={2}>2</LightBox>
+            <LightBox order={3}>3</LightBox>
+            <DarkBox order={1}>1</DarkBox>
+            <DarkBox order={4}>4</DarkBox>
+            </GridWrap>
+          </ContentWrapper>
+        </FlexContainer>
       </Layout>
     )
   }
 }
 
 export default IndexPage
-
-
-//subheadOne='March 27, 2020' subheadTwo='The Factory - Atlanta' 
-
-//Stuff for later
-/*
-        <ContentWrapper id='about'>
-          Here's some important stuff about the wedding
-        </ContentWrapper>
-        <ContentWrapper id='rsvp'>
-          RSVP Please
-        </ContentWrapper>
-        <ContentWrapper id='weddingParty'>
-          Look at these cool people
-        </ContentWrapper>
-*/
-
-/*
-        <ContentWrapper id='weddingParty'>
-          <Title title='Wedding Party' subPage={true} />
-        </ContentWrapper>
-        <ContentWrapper id='registry'>
-          <Title title='Registry' subPage={true} />
-        </ContentWrapper>
-        <ContentWrapper id='photos'>
-          <Title title='Photos' subPage={true} />
-        </ContentWrapper>
-
-
-*/
