2166713b01afb3723dbb294d0a03362786b7cac9
Hayden Sykes <hsykes@LOCUS.local>
2020-10-29 16:19:29 -0400

    created new rsvp component as a temporary fillin.  The actual component can be copied from startFromScratch

--- src/components/landing.js
+++ src/components/landing.js
@@ -190,11 +190,11 @@ const Landing = () => {
             
             <FlexContainer>
                 <ContentWrapper id='home' >
-                        <Header>Chloe & Hayden</Header>
-                        <Date>March 27, 2021</Date>    
-                        <Hashtag>#PsychedToBeSykes</Hashtag>        
-                        <DiscoBall src={discoGIF} alt="groovy disco ball" />
-                        <RsvpButton href="/rsvp">RSVP</RsvpButton>
+                    <Header>Chloe & Hayden</Header>
+                    <Date>March 27, 2021</Date>    
+                    <Hashtag>#PsychedToBeSykes</Hashtag>        
+                    <DiscoBall src={discoGIF} alt="groovy disco ball" />
+                    <RsvpButton href="/rsvp">RSVP</RsvpButton>
                 </ContentWrapper>
             </FlexContainer>
             
