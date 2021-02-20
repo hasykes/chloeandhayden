44e88e24512f8cd6e71401b5327f252c4a5ca0f1
Hayden Sykes <haydensykes13@gmail.com>
2021-02-20 10:56:08 -0500

    update to stateful component because I don't want to use hooks

--- src/pages/rehersal.js
+++ src/pages/rehersal.js
@@ -1,9 +1,21 @@
 // redirect for common misspelling
-import { navigate } from 'gatsby'
+import { navigate } from 'gatsby';
+import React from 'react';
 
-const Rehersal = () => {
-    navigate('/rehearsal')
-    return null
+//I should probably use hooks, but...
+class Rehersal extends React.Component{
+    constructor(){
+        super()
+    }
+
+    componentDidMount(){
+        navigate('/rehearsal')
+    }
+
+    render(){
+        return null
+    }
+    
 }
 
 export default Rehersal
